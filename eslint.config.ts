86e0230b8aeee4cac48572e6d7715e36044a321f
kazuya kawaguchi <kawakazu80@gmail.com>
2024-08-24 20:34:41 +0900

    setup lint and formatter

---
+++ eslint.config.ts
@@ -0,0 +1,42 @@
+import {
+  defineConfig,
+  javascript,
+  typescript,
+  comments,
+  promise,
+  unicorn,
+  regexp,
+  jsonc,
+  yml,
+  toml,
+  prettier,
+  jsdoc
+} from '@kazupon/eslint-config'
+
+const config: ReturnType<typeof defineConfig> = defineConfig(
+  javascript(),
+  comments(),
+  promise(),
+  regexp(),
+  unicorn(),
+  typescript(),
+  jsdoc({
+    typescript: 'syntax'
+  }),
+  jsonc({
+    json: true,
+    json5: true,
+    jsonc: true,
+    prettier: true
+  }),
+  yml({
+    prettier: true
+  }),
+  toml(),
+  prettier(),
+  {
+    ignores: ['tsconfig.json']
+  }
+)
+
+export default config
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+/**
+ * add
+ * @param a a
+ * @param b b
+ * @returns added value
+ */
+export function add(a: number, b: number): number {
+  return a + b
+}
+const _returnValue = add(1, 2)
 console.log('hi there!')
