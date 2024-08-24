ad11a9fad5ed87ce07282c86a72ac43275162d2f
kazuya kawaguchi <kawakazu80@gmail.com>
2024-08-25 03:22:14 +0900

    feat: add first observer utils

---
+++ src/observer/index.test.ts
@@ -0,0 +1,102 @@
+import { vi, expect, test } from 'vitest'
+import { observe } from './index.js'
+
+interface State {
+  a: number
+  obj?: {
+    foo: string
+  }
+}
+
+test('basic', () => {
+  const observer = observe<State>()
+
+  const listener = vi.fn()
+  const unsubscribe = observer.subscribe(listener)
+
+  expect(observer.listenerCount).toBe(1)
+
+  // call notify first time
+  observer.notify({ a: 1 })
+
+  expect(listener).toHaveBeenCalledTimes(1)
+  expect(listener.mock.calls[0][0]).toEqual({ a: 1 })
+
+  // call notify 2nd time
+  observer.notify({ a: 2, obj: { foo: 'bar' } })
+
+  expect(listener).toHaveBeenCalledTimes(2)
+
+  expect(listener.mock.calls[1][0]).toEqual({ a: 2, obj: { foo: 'bar' } })
+
+  // call notify, when it's unsubscribed
+  unsubscribe()
+  observer.notify({ a: 3 })
+
+  expect(observer.listenerCount).toBe(0)
+  expect(listener).toHaveBeenCalledTimes(2)
+})
+
+test('multiple subscribers', () => {
+  const observer = observe<State>()
+
+  const listener1 = vi.fn()
+  const listener2 = vi.fn()
+
+  const unsubscribe1 = observer.subscribe(listener1)
+  const unsubscribe2 = observer.subscribe(listener2)
+
+  expect(observer.listenerCount).toBe(2)
+
+  // call notify first time
+  observer.notify({ a: 1 })
+
+  expect(listener1).toHaveBeenCalledTimes(1)
+  expect(listener2).toHaveBeenCalledTimes(1)
+
+  expect(listener1.mock.calls[0][0]).toEqual({ a: 1 })
+
+  expect(listener2.mock.calls[0][0]).toEqual({ a: 1 })
+
+  // unsubscribe listener1 and call notify 2nd time
+  unsubscribe1()
+  observer.notify({ a: 3 })
+
+  expect(observer.listenerCount).toBe(1)
+  expect(listener1).toHaveBeenCalledTimes(1)
+  expect(listener2).toHaveBeenCalledTimes(2)
+
+  expect(listener2.mock.calls[1][0]).toEqual({ a: 3 })
+
+  // remaining listener also unsubscribe and call notify 3rd time
+  unsubscribe2()
+  observer.notify({ a: 2, obj: { foo: 'bar' } })
+
+  expect(observer.listenerCount).toBe(0)
+  expect(listener1).toHaveBeenCalledTimes(1)
+  expect(listener2).toHaveBeenCalledTimes(2)
+})
+
+test('dispose', () => {
+  const observer = observe<State>()
+
+  const listener1 = vi.fn()
+  const listener2 = vi.fn()
+
+  const unsubscribe1 = observer.subscribe(listener1)
+  const unsubscribe2 = observer.subscribe(listener2)
+
+  expect(observer.listenerCount).toBe(2)
+
+  // remove all listeners
+  observer.dispose()
+
+  expect(observer.listenerCount).toBe(0)
+  // check error does not occur, it has already been disposed
+  expect(() => {
+    unsubscribe1()
+  }).not.toThrow()
+  expect(() => {
+    unsubscribe2()
+  }).not.toThrow()
+})
---
+++ src/observer/index.ts
@@ -0,0 +1,80 @@
+/**
+ * State value subscribe handler
+ */
+export type ObservableStateSubscribeHandler<State> = (state: State) => void
+
+/**
+ * State value unsubscribe handler
+ */
+export type ObservableStateUnsubscribeHandler = () => void
+
+/**
+ * State value observable
+ */
+export interface Observable<State> {
+  /**
+   * listener count
+   */
+  readonly listenerCount: number
+  /**
+   * add state subscription
+   * @param listener listener
+   * @returns unsubscribe handler
+   */
+  subscribe(listener: ObservableStateSubscribeHandler<State>): ObservableStateUnsubscribeHandler
+  /**
+   * notify state value
+   * @param state updated state value
+   */
+  notify(state: State): void
+  /**
+   * dispose observable
+   */
+  dispose(): void
+}
+
+/**
+ * observe state value
+ * @returns observable state value
+ */
+export function observe<State>(): Readonly<Observable<State>> {
+  const listeners: ObservableStateSubscribeHandler<State>[] = []
+  let _listenerCount = 0
+
+  // eslint-disable-next-line jsdoc/require-jsdoc
+  function subscribe(listener: ObservableStateSubscribeHandler<State>) {
+    listeners.push(listener)
+    _listenerCount = listeners.length
+
+    const unsubscribe = () => {
+      const index = listeners.indexOf(listener)
+      if (index > -1) {
+        listeners.splice(index, 1)
+        _listenerCount = listeners.length
+      }
+    }
+    return unsubscribe
+  }
+
+  // eslint-disable-next-line jsdoc/require-jsdoc
+  function notify(state: State) {
+    for (const listener of listeners) listener(state)
+  }
+
+  /**
+   * TODO: ref: ECMAScript Explicit Resource Management
+   * https://github.com/tc39/proposal-explicit-resource-management
+   */
+  function dispose() {
+    _listenerCount = listeners.length = 0
+  }
+
+  return {
+    get listenerCount() {
+      return _listenerCount
+    },
+    subscribe,
+    notify,
+    dispose
+  }
+}
